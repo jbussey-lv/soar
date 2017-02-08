b24a83cda224b9e4fd567335665ded3ee45d8014
John Bussey <jbussey365@yahoo.com>
2017-02-07 21:24:56 -0500

    got things working with force module changed to use ES6 class syntax

--- public/js/Force.js
+++ public/js/Force.js
@@ -1,36 +1,18 @@
-var Force = (function(Vector){
+class Force {
 
-    var create = function(name, color, getPosition, getValue){
-
-        var name        = name || '';
-        var color       = color || '#CCC';
-        var getPosition = getPosition || function(){return Vector.create();}
-        var getValue    = getValue || function(){return Vector.create();}
-
-        var getName = function(){
-            return name;
-        }
-
-        var getColor = function(){
-            return color;
-        }
-
-        var getCogOffset = function(){
-
-            var cog_offset = Vector.difference(getPosition(), this.character.getCenterOfGravity())
-                                   .addAngle(orientation);
-
-            return cog_offset;
-        }
-
-        return {
-            getName,
-            getColor,
-            getPosition,
-            getValue,
-            getCogOffset
-        }
+    constructor(name, color, getPosition, getValue) {
+        this.name        = name || '';
+        this.color       = color || '#CCC';
+        this.getPosition = getPosition || function(){return Vector.create();}
+        this.getValue    = getValue || function(){return Vector.create();}
     }
 
-    return {create};
-}(Vector));
+    getName(){
+        return this.name;
+    }
+
+    getColor(){
+        return this.color;
+    }
+
+}
