981938b923df5a8506379f1d2d4641e34f88d0aa
John Bussey <jbussey365@yahoo.com>
2017-02-08 21:00:56 -0500

    added a grid for debugging positions. Adn got positioning working

--- public/js/Stage.js
+++ public/js/Stage.js
@@ -10,6 +10,33 @@ class Stage {
 
         this.initializeContainer();
         this.initializeWorld();
+        this.drawGuide();
+    }
+
+    drawGuide(){
+
+        var interval = 100;
+
+        // verticals
+        for(var i=interval; i<this.pixel_width; i+=interval){
+            this.addLine(i, 0, i, this.pixel_height);
+        }
+
+        // horizontals
+        for(var i=interval; i<this.pixel_height; i+=interval){
+            this.addLine(0, i, this.pixel_width, i);
+        }
+    }
+
+    addLine(x1, y1, x2, y2){
+        var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+        newLine.setAttribute('x1', x1);
+        newLine.setAttribute('y1', y1);
+        newLine.setAttribute('x2', x2);
+        newLine.setAttribute('y2', y2);
+        newLine.setAttributeNS(null, 'stroke-width', 1);
+        newLine.setAttributeNS(null, 'stroke', 'red');
+        this.dom_node.appendChild(newLine);
     }
 
     initializeContainer(){
@@ -72,14 +99,21 @@ class Stage {
 
     setPosition(character){
 
-        var position = character.position;
-        var angle    = character.orientation;
-        var cog      = character.cog;
-        var screen_x = this.metersToPixels(position.getX() + cog.getX());
-        var screen_y = this.metersToPixels(position.getY() + cog.getY());
+        var position    = character.position;
+        var angle       = character.orientation;
+        var cog         = character.cog;
+        var x           = position.getX();
+        var y           = position.getY();
+        var adjusted_x  = x - cog.getX();
+        var adjusted_y  = y - cog.getY();
 
-        character.dom_node.setAttribute('x', screen_x);
-        character.dom_node.setAttribute('y', screen_y);
+        var screen_x = this.metersToPixels(x);
+        var screen_y = this.metersToPixels(y);
+        var screen_adjusted_x = this.metersToPixels(adjusted_x);
+        var screen_adjusted_y = this.metersToPixels(adjusted_y);
+
+        character.dom_node.setAttribute('x', screen_adjusted_x);
+        character.dom_node.setAttribute('y', screen_adjusted_y);
         character.dom_node.setAttribute('transform', 'rotate('+angle+' '+screen_x+' '+screen_y+')');
 
     }
