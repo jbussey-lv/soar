8c6bdab6a84949052b9a5f481f36dca410964c1a
John Bussey <john@patterninc.com>
2021-08-25 22:02:43 -0400

    got vector class roughed in with tests running

--- test/vector.ts
+++ test/vector.ts
@@ -7,4 +7,8 @@ describe('Vector', () => {
     expect(v.x).to.equal(0);
     expect(v.y).to.equal(0);
   });
+  it('returs magnitude equal to hypotenous', () => {
+    const v = new Vector(3,4);
+    expect(v.magnitude).to.equal(5);
+  })
 });
