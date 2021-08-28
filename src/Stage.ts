7cb70f91a1d1ad0babc26fc2ee13b6db3201b318
John Bussey <john@patterninc.com>
2021-08-27 20:16:11 -0400

    got building properly

--- src/Stage.ts
+++ src/Stage.ts
@@ -6,6 +6,17 @@ export default class Stage {
   pixelHeight: number = 600;
   pixelsPerMeter: number = 10;
   origin: Vec = Vec.n(0, 0); // where bottom left of stage shows in real coords
+  svg: HTMLElement;
+
+  constructor(containerDiv: HTMLElement) {
+    this.svg = document.createElement('SVG');
+    this.svg.setAttribute('style', 'border: 1px solid black');
+    this.svg.setAttribute('width', '600');
+    this.svg.setAttribute('height', '250');
+    this.svg.setAttribute('version', '1.1');
+    this.svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    containerDiv.appendChild(this.svg);
+  }
 
   public getPaintPos(realMeterPos: Vec): Vec {
     let stageMeterPos = realMeterPos.subtract(this.origin);
