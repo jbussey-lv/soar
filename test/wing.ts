82f1883c91371bdfed8b11ce92b5502d6b51bb34
John Bussey <john@patterninc.com>
2021-09-23 01:39:22 -0400

    added more wing tests

--- test/wing.ts
+++ test/wing.ts
@@ -29,5 +29,16 @@ describe('Wing', () => {
       let force = wing.getForceMagnitude(1, Math.PI, 1);
       expect(force).to.almost.equal(0);
     });
+    it('shows some force at 45 degrees', () => {
+      let wing = getBasicWing();
+      let force = wing.getForceMagnitude(1, Math.PI / 4, 1);
+      expect(force).to.be.greaterThan(0);
+    });
+    it('shows more force at 90 degrees than at 45 degrees', () => {
+      let wing = getBasicWing();
+      let force90 = wing.getForceMagnitude(1, Math.PI / 2, 1);
+      let force45 = wing.getForceMagnitude(1, Math.PI / 4, 1);
+      expect(force90).to.be.greaterThan(force45);
+    });
   })
 });
