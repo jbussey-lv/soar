2007bace91f7d4e855fba7ed83c215dbfad5cd3d
John Bussey <john@patterninc.com>
2021-11-08 23:51:05 -0500

    implemented rk4. mostly eliminated explostions

--- src/AbstractObject.ts
+++ src/AbstractObject.ts
@@ -1,7 +1,7 @@
 import Vec from "./Vec";
 import { ForceArm } from "./ForceArm";
 import Setting from "./Setting";
-
+import * as _ from "lodash";
 
 export default abstract class AbstractObject {
 
@@ -31,18 +31,58 @@ export default abstract class AbstractObject {
   }
  
   updatePosition(dt: number) {
+    
+    let obj1 = _.cloneDeep(this);
+    obj1.pos = this.pos.clone();
+    obj1.vel = this.vel.clone();
+    obj1.ang = this.ang;
+    obj1.angVel = this.angVel;
+    let [acc1, angAcc1] = obj1.getAccs();
+
+    let obj2 = _.cloneDeep(this);
+    obj2.pos = this.pos.plus(obj1.vel.times(0.5 * dt));
+    obj2.vel = this.vel.plus(acc1.times(0.5 * dt));
+    obj2.ang = this.ang + 0.5 * obj1.angVel * dt;
+    obj2.angVel = this.angVel + 0.5 * angAcc1 * dt;
+    let [acc2, angAcc2] = obj2.getAccs();
+  
+    let obj3 = _.cloneDeep(this);
+    obj3.pos = this.pos.plus(obj2.vel.times(0.5 * dt));
+    obj3.vel = this.vel.plus(acc2.times(0.5 * dt));
+    obj3.ang = this.ang + 0.5 * obj2.angVel * dt;
+    obj3.angVel = this.angVel + 0.5 * angAcc2 * dt;
+    let [acc3, angAcc3] = obj3.getAccs();
+
+    let obj4 = _.cloneDeep(this);
+    obj4.pos = this.pos.plus(obj3.vel.times(dt));
+    obj4.vel = this.vel.plus(acc3.times(dt));
+    obj4.ang = this.ang + obj3.angVel * dt;
+    obj4.angVel = this.angVel + angAcc3 * dt;
+    let [acc4, angAcc4] = obj3.getAccs();
+  
+    this.pos = this.pos.plus((
+      obj1.vel.plus(obj2.vel.times(2))
+              .plus(obj3.vel.times(2))
+              .plus(obj4.vel))
+              .times(dt / 6)
+    );
+
+    this.vel = this.vel.plus((
+      acc1.plus(acc2.times(2))
+          .plus(acc3.times(2))
+          .plus(acc4))
+          .times(dt / 6)
+    );
+
+    this.ang = this.ang + (dt / 6) * (obj1.angVel + 2*obj2.angVel + 2*obj3.angVel + obj4.angVel);
+    this.angVel = this.angVel + (dt / 6) * (angAcc1 + 2*angAcc2 + 2*angAcc3 + angAcc4);
+  }
+
+  getAccs = function(): [Vec, number] {
     let forceArms = this.getAllForceArms();
     let acc = this.getNetForce(forceArms).divide(this.mass);
-    this.vel = this.vel.plus(acc.times(dt))
-    this.pos = this.pos.plus(this.vel.times(dt));
-
     let angAcc = this.getNetTorque(forceArms) / this.moment;
-    this.angVel += angAcc * dt;
-    this.ang = this.mod(this.ang + this.angVel * dt, 2*Math.PI);
-
-    if (isNaN(this.angVel) || isNaN(this.ang)) {
-      return 'Not a Number!';
-    }
+    return [acc, angAcc];
   }
 
   mod = function(number, base) {
